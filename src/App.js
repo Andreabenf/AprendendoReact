276074607fe03fa27591c8520f4fcd0c6f46279c
andreabenf <andreabenf@gmail.com>
2019-03-21 08:55:04 -0300

    Adicionada um componente chamado pessoa para testar novas possibilidades

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Pessoa from './Pessoa/Pessoa'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Aprendendo React</h1>
+        <Pessoa />
       </div>
     );
   }
---
+++ src/Pessoa/Pessoa.js
@@ -0,0 +1,10 @@
+import React from 'react';
+/* Na sua forma mais simples, um componente eh uma 
+    funcao que retorna um .jsx*/
+
+const pessoa = () => {
+    return <p>Eu sou uma pessoa</p>
+
+}
+
+export default pessoa;
