4fc6b0c61e210ad817014e5fd62aefdeb48c9988
andreabenf <andreabenf@gmail.com>
2019-03-21 11:35:52 -0300

    Mudando a class App pra ter states

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 import Pessoa from './Pessoa/Pessoa'
 
 class App extends Component {
+  state = {
+    persons: [
+      {carac: 'chata', age: 28},
+      {carac: 'feia', age: 22},
+      {carac: 'gorda', age: 12},
+      {carac: 'troxa', age: 34}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Aprendendo React</h1>
-        <Pessoa />
+        <Pessoa carac={this.state.persons[Math.floor(Math.random()*this.state.persons.length)].carac} age={this.state.persons[Math.floor(Math.random()*this.state.persons.length)].age}>Como isso sai?</Pessoa>
+        <button>Clica ai pra mudar os nomes</button>
       </div>
     );
   }
--- src/Pessoa/Pessoa.js
+++ src/Pessoa/Pessoa.js
@@ -1,10 +1,15 @@
 import React from 'react';
 /* Na sua forma mais simples, um componente eh uma 
     funcao que retorna um .jsx*/
+// props eh um objeto que recebemos como argumento
 
-const pessoa = () => {
-    return <p>Eu sou uma pessoa</p>
-
+const pessoa = (props) => {
+    return( 
+        <div>
+            <p>{props.children}</p>
+            <p>Eu sou uma pessoa muito {props.carac} e tenho {props.age} anos</p>
+        </div>
+    )
 }
 
 export default pessoa;
